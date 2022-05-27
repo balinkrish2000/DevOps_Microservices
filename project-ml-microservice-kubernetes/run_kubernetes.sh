a7822e0b04b6030f7a6f9c27530868226f0dbb83
EC2 Default User <ec2-user@ip-172-31-53-108.ec2.internal>
2022-05-27 14:40:22 +0000

    mlapp first push

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=balinkrish2000/udacityapi
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+docker login -u balinkrish2000
+kubectl run api --image=$dockerpath:api --port=8000
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward pod/api 8000:80
 
